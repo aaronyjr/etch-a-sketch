bc35bceb518021fc4a35f69777daa02243b223cd
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-08-27 08:03:00 +0800

    Change color of div on hover

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 let container = document.querySelector('.container')
+let size;
 
-function createGrid(rows, columns , sizeOfSquare) {
+function getSize() {
+    size = prompt('Size of grid (e.g "Type 16 for 16x16 grid)"');
+    while (isNaN(size)) {
+        size = prompt('Size of grid (e.g "Type 16 for 16x16 grid)"');
+    }
+    if (size <= 0) {
+        size = 1
+    } else if (size >= 100) {
+        size = 100
+    }
+}
+
+function createGrid(rows, columns) {
+    const sizeOfSquare = (500/rows).toString()
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
             let squareDiv = document.createElement("div");
+            squareDiv.addEventListener('mouseover', () => {
+                squareDiv.style.backgroundColor = 'blue';
+            })
             squareDiv.classList.add("squareDiv");
     
             squareDiv.style.left = j * sizeOfSquare + "px";
             squareDiv.style.top = i * sizeOfSquare + "px";
+            squareDiv.style.height = sizeOfSquare + "px";
+            squareDiv.style.width = sizeOfSquare + "px";
     
             container.appendChild(squareDiv);
-            container.style.width = (sizeOfSquare * rows).toString() + 'px';
+            container.style.width = "500px";
+            container.style.height = "500px";
         }
     }
 }
 
-createGrid(16, 16, 30);
-
+getSize()
+createGrid(size, size);
